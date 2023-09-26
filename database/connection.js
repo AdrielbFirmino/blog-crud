dc492ebd0f8e014014cd3bcbcad632ea1e3e0acb
Adriel <adrieldebfirmino@gmail.com>
2023-09-26 15:28:03 -0300

    Salvando artigos e Ajustando tempo no Sequelize

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize('guiapress', 'root', '55299',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
