abe877685d3d286efe4b79fcbdf673e32863da18
Adriel <adrieldebfirmino@gmail.com>
2023-10-06 11:16:08 -0300

    Edição de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -52,4 +52,39 @@ router.post("/articles/delete", (req, res) =>{
     }
 });
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+    if(isNaN(id)){
+        res.redirect("/admin/articles");
+    }
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", {article: article, categories: categories});
+            });
+        }else{
+            res.redirect("/admin/articles");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/articles");
+    });
+});
+
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category; // O erro se concentra na atualização da categoria
+
+    Article.update({title: title, body: body, categoryId: category, slug: slugify(title)}, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
+
 module.exports = router;
