2f46beba02dbd27bacb4af3ebd60dc427a313bc5
Adriel <adrieldebfirmino@gmail.com>
2023-10-12 11:56:04 -0300

    Autenticação do login

--- users/UsersController.js
+++ users/UsersController.js
@@ -35,4 +35,35 @@ router.post("/users/create", (req, res) =>{
     });
 });
 
+router.get("/admin/users/login", (req, res) => {
+    res.render("admin/users/login");
+});
+
+router.post("/authenticate", (req, res) =>{
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.findOne({
+        where: {
+            email: email,
+        }
+    }).then(user => {
+        if(user != undefined){ //se existir um usuário com este email
+            //Validar a senha
+            var correct = bcrypt.compareSync(password, user.password);
+            if(correct){
+                req.session.user = {
+                    id: user.id,
+                    email: user.email
+                }
+                res.json(req.session.user)
+            }else{
+                res.redirect("/login")
+            }
+        }else{
+            res.redirect("/login")
+        }
+    });
+});
+
 module.exports = router;
